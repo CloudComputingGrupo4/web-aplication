fa21d6a54dffbdc9038650d4c0ffce1082d4aea1
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-10-25 12:31:37 -0500

    featuring: Favicon.ico con fallas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 //src/app/app.routes
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
